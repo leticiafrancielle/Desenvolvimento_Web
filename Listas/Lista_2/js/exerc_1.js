fc5ba9a36d413fe3c6934b064bd10667265abff4
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-04-06 23:21:45 -0300

    exercícios desenvolvimento_web

---
+++ Listas/Lista_2/js/exerc_1.js
@@ -0,0 +1,24 @@
+var salarioAtual1 = 6000 
+var salarioAtual2 = 3000 
+var salarioAtual3 = 2000 
+var salarioAtual4 = 4000 
+var salarioAtual5 = 7000 
+var reajusteSalarial = 10 
+
+var valorReajuste1 = salarioAtual1 * reajusteSalarial / 100 
+var valorReajuste2 = salarioAtual2 * reajusteSalarial / 100 
+var valorReajuste3 = salarioAtual3 * reajusteSalarial / 100 
+var valorReajuste4 = salarioAtual4 * reajusteSalarial / 100 
+var valorReajuste5 = salarioAtual5 * reajusteSalarial / 100 
+
+var salarioFinal1 = salarioAtual1 + valorReajuste1 
+var salarioFinal2 = salarioAtual2 + valorReajuste2 
+var salarioFinal3 = salarioAtual3 + valorReajuste3 
+var salarioFinal4 = salarioAtual4 + valorReajuste4 
+var salarioFinal5 = salarioAtual5 + valorReajuste5 
+
+console.log("Adrian ganha R$", salarioAtual1, ", com reajuste de 10% passa a ganhar R$", salarioFinal1) 
+console.log("Mariana ganha R$", salarioAtual2, ", com reajuste de 10% passa a ganhar R$", salarioFinal2) 
+console.log("Juliana ganha R$", salarioAtual3, ", com reajuste de 10% passa a ganhar R$", salarioFinal3) 
+console.log("Eduarda ganha R$", salarioAtual4, ", com reajuste de 10% passa a ganhar R$", salarioFinal4) 
+console.log("Edgar ganha R$", salarioAtual5, ", com reajuste de 10% passa a ganhar R$", salarioFinal5) 
